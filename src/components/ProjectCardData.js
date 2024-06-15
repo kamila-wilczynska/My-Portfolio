0fe8c8e0cb5d4dc8c0eefd767e8e3167df0e8d36
Kamila W <kamila.wilczynska92@gmail.com>
2024-06-15 22:21:02 +0100

    add cards

--- src/components/ProjectCardData.js
+++ src/components/ProjectCardData.js
@@ -1,19 +1,55 @@
 import project1 from "./assets/images/project1.png"
 import project2 from "./assets/images/project2.png"
+import projectGroup1 from "./assets/images/cooking-assistant.png"
+import projectGroup2 from "./assets/images/dreamland.png"
+import projectGroup3 from "./assets/images/chaos-animals.png"
+import otherGrid from "./assets/images/grid.png"
+
 
 const ProjectCardData = [
 	{
+		category: "React Projects",
 		imgsrc: project1,
 		title: "Airbnb Project",
 		text: "Lorem ipsum",
 		view: "https://github.com/kamila-wilczynska/Airbnb"
 	},
 	{
+		category: "Other Projects",
 		imgsrc: project2,
 		title: "Random User API",
 		text: "Lorem ipsum",
 		view: "https://github.com/kamila-wilczynska/Api--Random-User"
-	}
+	},
+	{
+		category: "Group Projects",
+		imgsrc: projectGroup2,
+		title: "Dreamland",
+		text: "Lorem ipsum",
+		view: "https://github.com/kamila-wilczynska/Dream-Generator"
+	},
+	{
+		category: "Group Projects",
+		imgsrc: projectGroup1,
+		title: "AI Cooking Assistant",
+		text: "Lorem ipsum",
+		view: "https://github.com/kamila-wilczynska/Team-Wolf"
+	},
+	{
+		category: "Group Projects",
+		imgsrc: projectGroup3,
+		title: "Chaos Animals",
+		text: "Lorem ipsum",
+		view: "https://github.com/kamila-wilczynska/animal-chaos"
+	},
+	{
+		category: "Other Projects",
+		imgsrc: otherGrid,
+		title: "Learning Grid-Fotomatic",
+		text: "Lorem ipsum",
+		view: "https://github.com/kamila-wilczynska/Fotomatic-Codecademy"
+	},
+
 ]
 
 export default ProjectCardData;
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -1,34 +1,96 @@
+// import React from "react";
+// import "./Work.css"
+// import ProjectCardData from "../ProjectCardData";
+// import ProjectCard from "../ProjectCard/ProjectCard";
+
+
+
+// function Work() {
+// 	return (
+// 		<div className="work-container">
+// 			<h1 className="project-heading"> Projects</h1>
+// 			<div className="project-container">
+
+// 				{ProjectCardData.map((value, index) =>{
+// 					return(
+// 						<ProjectCard
+// 						key={index}
+// 						imgsrc = {value.imgsrc}
+// 						title= {value.title}
+// 						text= {value.text}
+// 						view= {value.view}
+
+
+// 						/>
+// 					)
+// 				})}
+
+
+// 			</div>
+// 		</div>
+// 	)
+// }
+
+// export default Work;
+
+
 import React from "react";
-import "./Work.css"
+import "./Work.css";
 import ProjectCardData from "../ProjectCardData";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
-
-
 function Work() {
+	// Filtruj dane na podstawie kategorii
+	const groupProjects = ProjectCardData.filter(project => project.category === "Group Projects");
+	const reactProjects = ProjectCardData.filter(project => project.category === "React Projects");
+	const otherProjects = ProjectCardData.filter(project => project.category === "Other Projects");
+
 	return (
 		<div className="work-container">
-			<h1 className="project-heading"> Projects</h1>
+
+			{/* Group Projects Section */}
+			<h2>Group Projects</h2>
 			<div className="project-container">
-
-				{ProjectCardData.map((value, index) =>{
-					return(
-						<ProjectCard
+				{groupProjects.map((value, index) => (
+					<ProjectCard
 						key={index}
-						imgsrc = {value.imgsrc}
-						title= {value.title}
-						text= {value.text}
-						view= {value.view}
+						imgsrc={value.imgsrc}
+						title={value.title}
+						text={value.text}
+						view={value.view}
+					/>
+				))}
+			</div>
 
+			{/* React Projects Section */}
+			<h2>React Projects</h2>
+			<div className="project-container">
+				{reactProjects.map((value, index) => (
+					<ProjectCard
+						key={index}
+						imgsrc={value.imgsrc}
+						title={value.title}
+						text={value.text}
+						view={value.view}
+					/>
+				))}
+			</div>
 
-						/>
-					)
-				})}
-
-
+			{/* Other Projects Section */}
+			<h2>React Projects</h2>
+			<div className="project-container">
+				{otherProjects.map((value, index) => (
+					<ProjectCard
+						key={index}
+						imgsrc={value.imgsrc}
+						title={value.title}
+						text={value.text}
+						view={value.view}
+					/>
+				))}
 			</div>
 		</div>
-	)
+	);
 }
 
 export default Work;
