5a5f7dd37defa9442274fb86d5f76b7847e1a933
Kamila W <kamila.wilczynska92@gmail.com>
2024-06-17 20:22:50 +0100

    update footer

--- src/reusableCoponents/Footer/Footer.js
+++ src/reusableCoponents/Footer/Footer.js
@@ -1,9 +1,9 @@
 import "./Footer.css"
 import React from "react";
 import { IoHomeOutline } from "react-icons/io5";
-import { MdOutlineSettingsPhone } from "react-icons/md";
 import { GoMail } from "react-icons/go";
 import { CiLinkedin } from "react-icons/ci";
+import { FaGithub } from "react-icons/fa6";
 
 function Footer() {
 	return (
@@ -16,21 +16,22 @@ function Footer() {
 							<p>Ealing Broadway, London</p>
 						</div>
 					</div>
-					<div className="phone">
-						<MdOutlineSettingsPhone size={25} className="icon-footer" />
-						<p>07902-176-308</p>
-					</div>
 					<div className="email">
 						<GoMail size={25} className="icon-footer" />
 						<p>kamila.wilczynska92@gmail.com</p>
+					</div>
+					<div className="social">
+						<div >
+							<a href="https://www.linkedin.com/in/kamila-wilczynska-7590141ab/"><CiLinkedin size={25} className="icon-footer" /></a>
+						</div>
+						<div >
+							<a href="https://github.com/kamila-wilczynska" className="social-left"><FaGithub size={25} className="icon-footer" /></a>
+						</div>
 					</div>
 				</div>
 				<div className="right">
 					<h4>About me</h4>
 					<p>Here is Kamila, junior web developer, career-changer. I enjoy discussing projects and design.</p>
-					<div className="social">
-						<CiLinkedin size={25} className="icon-footer" />
-					</div>
 				</div>
 			</div>
 		</div>
